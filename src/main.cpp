acd430c03dbf11e0ae2dc7dc70f4a84bfa93fb89
andrew <andrew@andrew-P35-DS3L.(none)>
2012-03-08 09:21:37 -0500

    lights

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,56 @@
  */
 
 #include "libtcod.hpp"
+#include "rad_shader.hpp"
+#include "player.h"
 #include <stdio.h>
 
+#define SCREEN_WIDTH 100
+#define SCREEN_HEIGHT 50 
 
-void init() {}
-void render() {}
+#define LIGHT_RADIUS 8 
+#define CELL_REFLECTIVITY 1.5
+#define CELL_SELF_ILLUMINATION 0.4
+
+TCODMap *map;
+Player *player;
+Shader *shader;
+
+int gamma[256];
+
+void init() 
+{
+    player = new Player();
+    map = new TCODMap(SCREEN_WIDTH,SCREEN_HEIGHT);
+    shader = new PhotonShader(CELL_REFLECTIVITY, CELL_SELF_ILLUMINATION, 10);
+    shader->addLight(player->getX(), player->getY(), LIGHT_RADIUS, TCODColor::lightAzure);
+    shader->addLight(player->getX() + 2, player->getY(), LIGHT_RADIUS, TCODColor::red);
+    shader->init(map);
+
+    for(int i=0; i<256; i++)
+            gamma[i] = i;
+}
+void render() 
+{
+    shader->compute();
+
+    for(int i = 0; i < SCREEN_WIDTH; i++)
+        for(int j = 0; j <SCREEN_HEIGHT; j++)
+        {
+            TCODColor light = shader->getLightColor(i,j) * TCODColor::darkAzure;
+            //TCODColor cellCol = TCODColor::lerp(TCODColor::black,light, gamma[light.r]/255.0f);
+            TCODConsole::root->setCharBackground(i,j,light);
+        }
+
+    //Draw player
+    TCODConsole::root->setCharForeground(player->getX(),player->getY(),player->getColor());
+    TCODConsole::root->setChar(player->getX(),player->getY(),player->getChar());
+}
 void update() {}
 
 int main(int argc, char** argv)
 {
-    TCODConsole::initRoot(40,40, "Darwin", false, TCOD_RENDERER_SDL);
+    TCODConsole::initRoot(SCREEN_WIDTH, SCREEN_HEIGHT, "Darwin", false, TCOD_RENDERER_SDL);
     TCODConsole::root->setAlignment(TCOD_CENTER);
     TCOD_key_t k = {TCODK_NONE,0};
     TCOD_mouse_t mouse;
--- src/player.cpp
+++ src/player.cpp
@@ -2,14 +2,22 @@
 
 Player::Player()
 {
-    x = 0;
-    y = 0;
+    x = 20;
+    y = 20;
     color = TCODColor::white;
+    ch = '@';
 }
 
-Player::Player(int i, int j, TCODColor c)
+Player::Player(int i, int j, TCODColor c, char chr)
 {
     x = i;
     y = j;
     color = c;
+    ch = chr;
+}
+
+void Player::move(int dx, int dy)
+{
+    x = x + dx;
+    y = y + dy;
 }
